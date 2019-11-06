2cad35676a9781e1ec863af70d5a1344647e1015
Ed Langley <el-github@elangley.org>
2019-11-05 22:39:34 -0800

    chore: reformat

--- fetching.lisp
+++ fetching.lisp
@@ -3,11 +3,11 @@
 
 (defmacro setup-libraries-for-feeds (&body body)
   `(let ((plump:*tag-dispatchers* plump:*xml-tags*)
-	 (drakma:*drakma-default-external-format* :utf-8)
-	 (drakma:*text-content-types* 
-	  (pairlis '("application" "application" "application")
-		   '("atom+xml"    "rss+xml"     "xml")
-		   drakma:*text-content-types*)))
+         (drakma:*drakma-default-external-format* :utf-8)
+         (drakma:*text-content-types*
+           (pairlis '("application" "application" "application")
+                    '("atom+xml"    "rss+xml"     "xml")
+                    drakma:*text-content-types*)))
      ,@body))
 
 (defvar *user-agent* "alimenta/0.0")
@@ -38,47 +38,48 @@
 
 (defun feed-ambiguous (choices)
   (error 'feed-ambiguous
-	 :choices choices))
+         :choices choices))
 
 (defun no-feed (url)
   (cerror "Skip this feed" 'no-feed :url url))
 
 (defun fetch-feed-from-url (url &key type)
   (setup-libraries-for-feeds
-    (let* ((feeds (alimenta.discover:discover-feed (drakma:http-request url
-									:user-agent *user-agent*
-									:decode-content t)))
-	   (feeds (if type (remove-if-not (lambda (x) (eql type (car x)))
-					  feeds)
-		      feeds)))
+    (let* ((feeds (alimenta.discover:discover-feed
+                   (drakma:http-request url
+                                        :user-agent *user-agent*
+                                        :decode-content t)))
+           (feeds (if type (remove-if-not (lambda (x) (eql type (car x)))
+                                          feeds)
+                      feeds)))
       (if (not feeds)
-	  (no-feed url)
-	  (fetch-doc-from-url
-	   (cdar 
-	    (restart-case
-		(if (cdr feeds) (feed-ambiguous feeds) feeds)
-	      (take-first-feed nil
-		:report (lambda (s) (format s "Take the first feed"))
-		feeds)
-	      (take-nth-feed (n)
-		:report (lambda (s) (format s "Take the nth feed"))
-		(list (elt feeds n)))
-	      (select-feed (selector)
-		:report (lambda (s) (format s "Provide a function to select the right feed"))
-		(find-if selector feeds)))))))))
+          (no-feed url)
+          (fetch-doc-from-url
+           (cdar 
+            (restart-case
+                (if (cdr feeds) (feed-ambiguous feeds) feeds)
+              (take-first-feed nil
+                :report (lambda (s) (format s "Take the first feed"))
+                feeds)
+              (take-nth-feed (n)
+                :report (lambda (s) (format s "Take the nth feed"))
+                (list (elt feeds n)))
+              (select-feed (selector)
+                :report (lambda (s) (format s "Provide a function to select the right feed"))
+                (find-if selector feeds)))))))))
 
 (defmacro with-retry ((retry-message &rest args) action &body other-restarts)
   `(loop (restart-case (return ,action)
-	   (retry ()
-	     :report (lambda (s) (format s ,retry-message ,@args)))
-	   ,@other-restarts)))
+           (retry ()
+             :report (lambda (s) (format s ,retry-message ,@args)))
+           ,@other-restarts)))
 
 (defun pull-feed (url &key detect type)
   (with-retry ("Retry fetching feed ~a" url)
-      (to-feed
-       (if detect
-	   (fetch-feed-from-url url)
-	   (fetch-doc-from-url url)) 
-       :type type)
+              (to-feed
+               (if detect
+                   (fetch-feed-from-url url)
+                   (fetch-doc-from-url url)) 
+               :type type)
     (skip-feed () (return)
-	       :report (lambda (s) (format s "Skip fetching feed ~a" url)))))
+               :report (lambda (s) (format s "Skip fetching feed ~a" url)))))
